fe71cf1343cfc8a312cfcdf2dc0f8e9dc87558c5
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-02-18 11:13:01 -0500

    js file edited

--- script.js
+++ script.js
@@ -4,25 +4,22 @@ let todoTable = document.querySelector(".todoTable");
 let removeBtn = document.querySelector(".removeBtn");
 
 class App {
-  inputValue;
+  inputValue = "";
   constructor() {
-    todoBtn.addEventListener("click", this.getValue.bind(this));
     todoBtn.addEventListener("click", this.postTodo.bind(this));
     removeBtn.addEventListener("click", this.removeTodo.bind(this));
   }
-  getValue() {
-    this.inputValue = todoInput.value;
-    if (this.inputValue === "") alert("Write please!");
-  }
   postTodo() {
+    this.inputValue = todoInput.value;
+    if (this.inputValue === "") return alert("Invalid");
     const html = `
     <div class="form-check form-switch check-input">
-    <input
-      class="form-check-input"
-      type="checkbox"
-      role="switch"
-      id="flexSwitchCheckDefault"
-    />
+      <input
+        class="form-check-input"
+        type="checkbox"
+        role="switch"
+        id="flexSwitchCheckDefault"
+      />
     <label class="form-check-label" for="flexSwitchCheckDefault"
       ><h3>${this.inputValue}</h3></label
     >
@@ -32,11 +29,15 @@ class App {
     todoInput.value = "";
     removeBtn.style.display = "block";
   }
+
   removeTodo() {
     let toRemove = Array.from(document.querySelectorAll(".check-input"));
     toRemove.forEach((el) => {
       if (el.children[0].checked) el.remove();
     });
+    if (toRemove.length <= 1) {
+      removeBtn.style.display = "none";
+    }
   }
 }
 
