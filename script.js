d11519faedff7e4f8f891808244af89f4a8eca2c
Lamadrid-Mike <mikealamadrid@hotmail.com>
2023-03-14 20:46:35 -0400

    finally working

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ let removeBtn = document.querySelector(".removeBtn");
 class App {
   inputValue = "";
   constructor() {
+    todoInput.addEventListener("keypress", this.postTodoOnKeyPress.bind(this));
     todoBtn.addEventListener("click", this.postTodo.bind(this));
     removeBtn.addEventListener("click", this.removeTodo.bind(this));
   }
+
+  postTodoOnKeyPress(e) {
+    if (e.key === "Enter") this.postTodo();
+  }
+
   postTodo() {
     this.inputValue = todoInput.value;
     if (this.inputValue === "") return alert("Invalid");
@@ -32,12 +38,12 @@ class App {
 
   removeTodo() {
     let toRemove = Array.from(document.querySelectorAll(".check-input"));
+    if (toRemove.every((el) => el.children[0].checked))
+      removeBtn.style.display = "none";
+
     toRemove.forEach((el) => {
       if (el.children[0].checked) el.remove();
     });
-    if (toRemove.length <= 1) {
-      removeBtn.style.display = "none";
-    }
   }
 }
 
